53a7454ee5bec7e964139e6b8b4c568284c35515
Caedamon <156080309+Caedamon@users.noreply.github.com>
2024-09-16 13:18:53 +0200

    upg 6
    done

---
+++ P6DataTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+String outputString = "Hello World";
+Console.WriteLine(outputString);
+// Double decimalValue = 0.3; <-- origin?
+float decimalValue = 0.3f; //för mindre värden
+double decimalVa = 0.3d; //för störe decimal värden
+Console.WriteLine(decimalValue);
+Boolean isAwesome = true;
+Console.WriteLine(isAwesome);
+    
+
+
+/*
+Create a Console Project named P6DataTypes How To?
+Assign the value Hello World to a variable named outputString
+Then print it to the Console
+Assign 0.3 to a float variable named decimalValue
+Print it to the Console
+Assign the value true to a bool variable named isAwesome
+Print it to the Console
+*/
