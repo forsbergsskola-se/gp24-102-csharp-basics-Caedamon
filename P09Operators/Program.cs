1c8f5bc08ffb05656f88a696589c5eca19b25413
Caedamon <156080309+Caedamon@users.noreply.github.com>
2024-09-16 14:01:37 +0200

    10
    break

---
+++ P09Operators/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ https://github.com/marczaku/102-csharp-basics/tree/main/09-operators
+ */
+
+
+Console.WriteLine("Hello, World!");
