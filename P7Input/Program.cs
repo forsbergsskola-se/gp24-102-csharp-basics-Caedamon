a9e2a497598d275fc3363c483f34ca36fcf74be8
Caedamon <156080309+Caedamon@users.noreply.github.com>
2024-09-16 13:23:37 +0200

    upg 7
    Done

---
+++ P7Input/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.Metadata;
+
+Console.WriteLine("Hello There! What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello {name}!");
+
+
+/*
+Create a Console Project named P7Input How To?
+Ask the user for their name
+Assign the User's Input to a variable named name
+Greet the user using his name
+Bonus: Use String InterpolationCreate a Console Project named P7Input How To?
+Ask the user for their name
+Assign the User's Input to a variable named name
+Greet the user using his name
+Bonus: Use String Interpolation
+ */
