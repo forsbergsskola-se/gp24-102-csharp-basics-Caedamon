beabc5247d375636ab828cd00a54e2ef99679243
Caedamon <156080309+Caedamon@users.noreply.github.com>
2024-09-17 14:05:35 +0200

    uppd.
    just added Fredrik W's comment in the discord chat to upg. 9

--- P09Operators/Program.cs
+++ P09Operators/Program.cs
@@ -6,3 +6,56 @@
 
 
 Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* --- all this is Fredriks Recap comment in the chat... this way i dont have to switch window if i wanna check it all the time xD
+ 
+// Slide 9 - Operator Recap:
+
+// doing any mathematical operation is quite easy, and works the same way as you'd expect any mathematical expression outside of code
+// addition can be done this way:
+
+int one = 1;
+int two = 2;
+int three = one + two; // can be done with two variables
+int threeAgain = one + 2; // or using a variable plus a number directly.
+int threeAgain2 = 1 + 2; // or two numbers directly.
+one += two; // adds one together with two for the sum of 3
+
+// Equally multiplication, subtraction or division are possible the same way:
+
+int four = 4;
+int fourTimesTwo = four * 2;
+float fourDividedByThree = four / 3f; // watch out, dividing two whole numbers can result in loss of precision, specify one as float or double
+int negativeOne = one - two;
+
+// Modulo operator (the percent sign %) is used to get the remainder of a division
+int remainderOf = 10 % 3; // equals 1 as 3*3+1 = 10
+// It can also be used to determine if a number is odd or even by using number % 2
+int remainderOfModulo = 9 % 2;
+bool isOdd = remainderOfModulo > 0;
+
+if (isOdd) {
+    Console.WriteLine("The number is odd!");
+} else if (!isOdd) {
+    Console.WriteLine("The number is even!");
+}
+
+// Some smart operators can help you write less code like the pre and post increment / decrement operators.
+// You use them as follows:
+int nine = 9;
+Console.WriteLine($"Incrementing AFTER I read the value of Nine: {nine++}");
+Console.WriteLine($"Reading the value of Nine: {nine}");
+Console.WriteLine($"Decrementing Nine BEFORE reading it: {--nine}");
+ */
