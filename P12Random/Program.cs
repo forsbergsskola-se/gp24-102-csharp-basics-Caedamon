db0ed786142114e938d67701bd5dcbc2c7a2b393
Caedamon <156080309+Caedamon@users.noreply.github.com>
2024-09-23 11:39:26 +0200

    P12
    Done and done.
    On to the hell that will be 13!

--- P12Random/Program.cs
+++ P12Random/Program.cs
@@ -1,18 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-/*
-    Instructions
-    Create a Console Project named P12Random How To?
-    Ask the User for a Seed and store it in a variable.
-    Initialize Random with the given Seed.
-    Get three Random Numbers between 0 and 5 (including 0, excluding 5) and print them to the Console.
-    Get three Random Numbers between 0 and 0.5 (including 0, excluding 0.5) and print them to the Console.
-    Get three Random Numbers between 0.2 and 0.7 (including 0.2, excluding 0.7) and print them to the Console.
-    Ask the user for a Critical Hit Chance between 0.0 (0%) and 1.0 (100%).
-    Then simulate 5 Attacks where you roll for the Critical Hit Chance and print, whether it’s a Critical Hit. Or No Critical Hit.
-    Hint: Thanks to using the Seed, you should actually receive the same results as me, if using the same Seed. Test it! :)
-*/
-
-Console.WriteLine("This is your Friendly AI-Borg");
+﻿Console.WriteLine("This is your Friendly AI-Borg");
 Console.WriteLine("We are doing battle today!");
 Console.WriteLine("We Are The Borg.");
 Console.WriteLine("Resistance is Futile, you Will be Assimilated!");
@@ -68,13 +54,9 @@ for (int i = 0; i < 5; i++)
 {
  double roll = random.NextDouble();
  if (roll < critChance)
- {
   Console.WriteLine("Attack {0}: Critical Hit!", i + 1);
- }
  else
- {
   Console.WriteLine("Attack {0}: No Critical Hit.", i + 1);
- }
 }
 
 Console.WriteLine("Press any key to continue...");
@@ -83,7 +65,6 @@ Console.Clear();
 
 //
 //P12_1Random_Coordinates
-//Develop a program that generates random X and Y coordinates for an enemy in a 2D game world with 100x100 coordinates. Display these coordinates to the user.
 //
 
 Console.WriteLine("This is your Friendly AI-Borg");
@@ -116,8 +97,7 @@ Console.Clear();
 
 //
 // P12_2Random_Password
-// Design a program that generates a random password with 6 characters, digits or symbols.
-//
+// 
 
 Console.WriteLine("We Are The Borg.");
 Console.WriteLine("Resistance is Futile, you Will be Assimilated!");
@@ -153,8 +133,7 @@ Console.Clear();
 
 //
 // P12_3Random_Item
-// Write a program that simulates item drops in a game. Define five different items, and randomly select an item for the player to acquire.
-//
+// 
 
 Console.WriteLine("We Are The Borg.");
 Console.WriteLine("Resistance is Futile, If you havent already been assimilated, you WILL be!");
@@ -195,14 +174,16 @@ Console.Clear();
 
 //
 // P12_4Random_Chance
-// Write a program that has a 10% chance of showing a secret message. Otherwise it just displays a message saying to try again.
-//
+// 
 
-//Console.WriteLine("We Are The Borg.");
-//Console.WriteLine("Resistance is Futile, you are assimilated, right?");
-//Console.WriteLine("");
+Console.WriteLine("We Are The Borg.");
+Console.WriteLine("Resistance is Futile, you are assimilated, right?");
+Console.WriteLine("Lets play a game! If your lucky, it will give you a secret message!");
 
+Random randomMsg = new Random();
+double chanceMsg = randomMsg.NextDouble();
 
-//Console.WriteLine("Press any key to continue...");
-//Console.ReadKey();
-//Console.Clear();
+if (chanceMsg < 0.1)
+ Console.WriteLine("Secret Message: You found the hidden treasure! You won, go buy a lottery ticket!");
+else
+ Console.WriteLine("Better luck next time, Human!");
