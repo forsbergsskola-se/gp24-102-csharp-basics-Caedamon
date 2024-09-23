2b9e226b9e6bcd5c248a657ef2a45b3caf1364ed
Caedamon <156080309+Caedamon@users.noreply.github.com>
2024-09-23 07:27:34 +0200

    Starting p12
    Fixing up, lining up, starting up

--- P09Operators/obj/Debug/net8.0/P09Operators.AssemblyInfo.cs
+++ P09Operators/obj/Debug/net8.0/P09Operators.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P09Operators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d6f49786f46243f825045c6a6b9cbf1a91b81c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5b351c1729417e624ab1b6342afb9d89cda803")]
 [assembly: System.Reflection.AssemblyProductAttribute("P09Operators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P09Operators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- P12Random/Program.cs
+++ P12Random/Program.cs
@@ -1,2 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+    Instructions
+    Create a Console Project named P12Random How To?
+    Ask the User for a Seed and store it in a variable.
+    Initialize Random with the given Seed.
+    Get three Random Numbers between 0 and 5 (including 0, excluding 5) and print them to the Console.
+    Get three Random Numbers between 0 and 0.5 (including 0, excluding 0.5) and print them to the Console.
+    Get three Random Numbers between 0.2 and 0.7 (including 0.2, excluding 0.7) and print them to the Console.
+    Ask the user for a Critical Hit Chance between 0.0 (0%) and 1.0 (100%).
+    Then simulate 5 Attacks where you roll for the Critical Hit Chance and print, whether it’s a Critical Hit. Or No Critical Hit.
+    Hint: Thanks to using the Seed, you should actually receive the same results as me, if using the same Seed. Test it! :)
+*/
+
 Console.WriteLine("Hello, World!");
+  
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+Console.Clear();
+
+/*
+ * P12_1Random_Coordinates
+ * Develop a program that generates random X and Y coordinates for an enemy in a 2D game world with 100x100 coordinates. Display these coordinates to the user.
+ */
+
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+Console.Clear();
+
+/*
+ * P12_2Random_Password
+ * Design a program that generates a random password with 6 characters, digits or symbols.
+*/
+
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+Console.Clear();
+
+/*
+ * P12_3Random_Item
+ * Write a program that simulates item drops in a game. Define five different items, and randomly select an item for the player to acquire.
+*/
+
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+Console.Clear();
+
+/*
+ * P12_4Random_Chance
+ * Write a program that has a 10% chance of showing a secret message. Otherwise it just displays a message saying to try again.
+*/
+
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+Console.Clear();
